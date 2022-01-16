7c1a3e0654b6d7edc1a91fd19b9641a0169042af
Aryan <aryannaidu010@gmail.com>
2022-01-16 16:57:43 +0530

    spawning tile

--- Assets/scripts/Board.cs
+++ Assets/scripts/Board.cs
@@ -1,8 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.VFX;
+using  UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Board : MonoBehaviour
 {
+  public Tilemap tilemap { get; private set; }
   public TetrominoData[] tetrominoes;
+  public Peice activePeice { get; private set; }
+  public Vector3Int activePosition;
+
+  private void Awake()
+  {
+    this.tilemap = GetComponentInChildren<Tilemap>();    //to get the tilemap which is a children of go.
+    this.activePeice = GetComponentInChildren<Peice>();
+    for (int i = 0; i < tetrominoes.Length; i++)
+    {
+      this.tetrominoes[i].Initialize();
+    }
+  }
+
+  private void Start()
+  {
+    spawn();
+  }
+
+  public void spawn()
+  {
+    int random = Random.Range(0, this.tetrominoes.Length);
+    TetrominoData data = this.tetrominoes[random];
+    this.activePeice.Initialize(this,activePosition ,data);
+    set(this.activePeice);
+  }
+
+  public void set(Peice peice)
+  {
+    for (int i = 0; i < peice.cells.Length; i++)
+    {
+      Vector3Int tilePosition = peice.cells[i]+peice.position;
+      this.tilemap.SetTile(tilePosition,peice.data.tile);
+    }
+    
+  }
 }
---
+++ Assets/scripts/Peice.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peice : MonoBehaviour
+{
+   public Board board { get; private set; }
+   public Vector3Int position { get; private set; }
+   public TetrominoData data { get; private set; }
+   public Vector3Int[] cells { get; private set; }
+   
+   public void Initialize(Board board, Vector3Int position,TetrominoData data)
+   {
+      this.board = board;
+      this.position = position;
+      this.data = data;
+      if (cells == null)
+      {
+         this.cells = new Vector3Int[4];
+      }
+
+      for (int i = 0; i < 4; i++)
+      {
+         this.cells[i] =(Vector3Int) data.cells[i];
+      }
+   }
+}
